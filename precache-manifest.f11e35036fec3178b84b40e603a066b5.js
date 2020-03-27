bf0f5bd87a8a715477ce284f0ec7bba5eee95b54
KainyRyu <KainyRyu@users.noreply.github.com>
2020-03-27 19:53:46 +0000

    Updates

---
+++ precache-manifest.f11e35036fec3178b84b40e603a066b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38d7349381b663e076cc4f601bceba2f",
+    "url": "/daily_report/index.html"
+  },
+  {
+    "revision": "24f2b9ec938bfb3b2eed",
+    "url": "/daily_report/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fbe74a41f4d736b6493a",
+    "url": "/daily_report/static/js/2.660caf7b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/daily_report/static/js/2.660caf7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24f2b9ec938bfb3b2eed",
+    "url": "/daily_report/static/js/main.adcfc5af.chunk.js"
+  },
+  {
+    "revision": "1079e395515531fe6ebd",
+    "url": "/daily_report/static/js/runtime-main.30a41e5a.js"
+  }
+]);
