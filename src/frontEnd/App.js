47d3d49c297fb85a3ab92fcd626a205796cdf701
KainyRyu <KainyRyu@users.noreply.github.com>
2020-03-31 17:16:14 +0100

    skeleton

--- src/frontEnd/App.js
+++ src/frontEnd/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import './App.css';
+import Timer from './components/Timer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Timer />
       </header>
     </div>
   );
---
+++ src/frontEnd/components/Timer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './timer.css';
+
+function timerFc() {
+    const = [ timeSetting, setTimesetting ] = useState(0)
+}
+
+
+export default function Timer() {
+    return (
+        <div className="Timer">
+            <div className="Timer-circle"></div>
+            <div className="Timer-circle-overlay"></div>
+        </div>
+    )
+}
