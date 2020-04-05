a0b5b8edde7c2117248b88543e8a6abc426daa91
KainyRyu <KainyRyu@users.noreply.github.com>
2020-04-05 22:31:20 +0100

    Getting current date

--- src/frontEnd/components/Today.js
+++ src/frontEnd/components/Today.js
@@ -2,9 +2,21 @@ import React from 'react';
 import './priority.css';
 
 export default function Today() {
+    
+    function today() {
+        let newDate = new Date();
+        let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Satuday'];
+        let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        let day = days[newDate.getUTCDay()];
+        let date = newDate.getUTCDate();
+        let month = months[newDate.getUTCMonth()];
+        let year = newDate.getUTCFullYear();
+        return `${day}, ${date} ${month} ${year}`
+    }
+
     return (
         <>
-            <div className="main-today">Thursday 2nd April 2020</div>
+            <div className="main-today">{today()}</div>
         </>
     )
 }
