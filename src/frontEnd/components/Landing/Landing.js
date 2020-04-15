f2e3d516b32253d5c1f31d50474aea525a9a200b
KainyRyu <KainyRyu@users.noreply.github.com>
2020-04-16 00:10:15 +0100

    register input form

--- src/frontEnd/components/Landing/Landing.js
+++ src/frontEnd/components/Landing/Landing.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react'
+import { Typography, Paper, Avatar, Button } from '@material-ui/core';
+import VerifiedUserOutlined from '@material-ui/icons/VerifiedUserOutlined';
+import withStyles from '@material-ui/core/styles/withStyles'
+import { Link } from 'react-router-dom';
 import Logo from '../../image/DR.svg';
 import './landing.css';
 import firebase from '../../utils/firebase';
+import Signup from '../Signup';
 
 
 
@@ -10,8 +15,27 @@ export default function Landing() {
     return (
         <div className="Landing">
             <img className="landing_logo" src={Logo} alt="logo" />
-            <button className="auth sign-in" onClick={''}>Login</button>
-            <button className="auth sign-up" onClick={''}>Sign Up</button>
+            <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="secondary"
+                component={Link}
+                to="/login"
+            >
+                Login
+            </Button>
+            <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="secondary"
+                component={Link}
+                to="/signup"
+            >
+                Sign Up
+            </Button>
+            <Signup />
                 {/* <button onClick={() => app.auth()></button> */}
         </div>
     )
--- src/frontEnd/components/Signup.js
+++ src/frontEnd/components/Signup.js
@@ -1,50 +1,82 @@
 import React, { useState, useEffect } from 'react';
+import { Typography, Paper, Avatar, Button, FormControl, Input, InputLabel } from '@material-ui/core';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import withStyles from '@material-ui/core/styles/withStyles'
+import { Link, withRouter } from 'react-router-dom';
 import firebase from '../utils/firebase';
 
-const Signup = () =>  {
-    const [name, setName] = useState("")
-    const [type, setType] = useState("")
-    const [qty, setQty] = useState(null)
-    const [description, setDescription] = useState("")
+const Signup = (props) =>  {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
 
-    const onSubmit = e => {
-        e.preventDefault()
-        firebase
-            .firestore()
-            .collection("items")
-            .add({
-                name,
-                type,
-                qty,
-                description,
-            })
-            .then(() => setName(""), setType(""), setQty(""), setDescription(""))
-    }
+
+    // const onSubmit = e => {
+    //     e.preventDefault()
+    //     firebase
+    //         .firestore()
+    //         .collection("account")
+    //         .add({
+    //             name,
+    //             email,
+    //             password,
+    //         })
+    //         .then(() => setName(""), setEmail(""), setPassword(""))
+    // }
 
     return (
-        <from onSubmit={onSubmit}>
-            <input 
-                placeholder="Name" 
-                value={name} 
-                name="name" 
-                type="text" />
-            <input 
-                placeholder="Type" 
-                value={type} 
-                name="type" 
-                type="text" />
-            <input 
-                placeholder="Qty" 
-                value={qty} 
-                name="qty" 
-                type="number" />
-            <input 
-                placeholder="Description" 
-                value={description} 
-                name="description" 
-                type="text" />
-            <button>Submit</button>
-        </from>
+        <form onSubmit={e => e.preventDefault() && false}>
+            <FormControl margin="normal" required fullWidth>
+                <InputLabel htmlFor="name">Name</InputLabel>
+                <Input 
+                    id="name"
+                    name="name"
+                    autoComplete="off"
+                    autoFocus
+                    value={name}
+                    onChange={e => setName(e.target.value)}
+                />
+            </FormControl>
+            <FormControl margin="normal" required fullWidth>
+                <InputLabel htmlFor="email">Email</InputLabel>
+                <Input 
+                    id="email"
+                    name="email"
+                    autoComplete="off"
+                    autoFocus
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                />
+            </FormControl>
+            <FormControl margin="normal" required fullWidth>
+                <InputLabel htmlFor="password">Password</InputLabel>
+                <Input  
+                    id="password"
+                    name="password"
+                    autoComplete="off"
+                    autoFocus
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                />
+            </FormControl>
+            <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                onClick={onRegister}
+            >
+                Submit
+            </Button>
+        </form>
     )
+    async function onRegister() {
+        try {
+            await firebase.register(name, email, password)
+            props.history.replace('/dashboard')
+        } catch(error) {
+            alert(error.message)
+        }
+    }
 }
 export default Signup;
