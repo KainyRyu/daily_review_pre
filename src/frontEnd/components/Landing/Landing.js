e96d039382ba73c91427d8a390c39eafce8e5577
KainyRyu <KainyRyu@users.noreply.github.com>
2020-04-16 12:10:33 +0100

    experiment

--- src/frontEnd/components/Landing/Landing.js
+++ src/frontEnd/components/Landing/Landing.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Typography, Paper, Avatar, Button } from '@material-ui/core';
 import VerifiedUserOutlined from '@material-ui/icons/VerifiedUserOutlined';
 import withStyles from '@material-ui/core/styles/withStyles'
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import { Link } from 'react-router-dom';
 import Logo from '../../image/DR.svg';
 import './landing.css';
@@ -11,7 +12,25 @@ import Signup from '../Signup';
 
 
 export default function Landing() {
-    
+    const [isSignedIn, setIsSignedIn] = useState(false);
+
+    const uiConfig = {
+        signInFlow: "popup",
+        signInOptions: [
+            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+            firebase.auth.EmailAuthProvider.PROVIDER_ID
+        ],
+        callbacks: {
+            signInSuccess: () => false
+        }
+    }
+
+    useEffect(() => {
+        firebase.auth().onAuthStateChanged(user => {
+            setIsSignedIn(!isSignedIn)
+        })
+    }, [])
+
     return (
         <div className="Landing">
             <img className="landing_logo" src={Logo} alt="logo" />
@@ -35,7 +54,14 @@ export default function Landing() {
             >
                 Sign Up
             </Button>
-            <Signup />
+            {isSignedIn ? (
+                <div>Signed in!</div>
+            ) : (
+                <StyledFirebaseAuth
+                    uiConfig={uiConfig}
+                    firebaseAuth={firebase.auth()}
+                />
+            )}
                 {/* <button onClick={() => app.auth()></button> */}
         </div>
     )
--- src/frontEnd/components/Signup.js
+++ src/frontEnd/components/Signup.js
@@ -1,82 +1,113 @@
-import React, { useState, useEffect } from 'react';
-import { Typography, Paper, Avatar, Button, FormControl, Input, InputLabel } from '@material-ui/core';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import withStyles from '@material-ui/core/styles/withStyles'
-import { Link, withRouter } from 'react-router-dom';
-import firebase from '../utils/firebase';
+import React, { useState, useEffect } from "react";
+import {
+  Typography,
+  Paper,
+  Avatar,
+  Button,
+  FormControl,
+  Input,
+  InputLabel,
+} from "@material-ui/core";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import withStyles from "@material-ui/core/styles/withStyles";
+import { Link, withRouter } from "react-router-dom";
+import firebase from "../utils/firebase";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 
-const Signup = (props) =>  {
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+const styles = (theme) => ({
+    main: {
+    width: "auto",
+    display: "block", // Fix IE 11 issue.
+    marginLeft: theme.spacing.unit * 3,
+    marginRight: theme.spacing.unit * 3,
+    [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+        width: 400,
+        marginLeft: "auto",
+        marginRight: "auto",
+    },
+    },
+    form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing.unit,
+    },
+    submit: {
+    marginTop: theme.spacing.unit * 3,
+    },
+});
 
+const Signup = (props) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    // const onSubmit = e => {
-    //     e.preventDefault()
-    //     firebase
-    //         .firestore()
-    //         .collection("account")
-    //         .add({
-    //             name,
-    //             email,
-    //             password,
-    //         })
-    //         .then(() => setName(""), setEmail(""), setPassword(""))
-    // }
-
-    return (
-        <form onSubmit={e => e.preventDefault() && false}>
-            <FormControl margin="normal" required fullWidth>
-                <InputLabel htmlFor="name">Name</InputLabel>
-                <Input 
-                    id="name"
-                    name="name"
-                    autoComplete="off"
-                    autoFocus
-                    value={name}
-                    onChange={e => setName(e.target.value)}
-                />
-            </FormControl>
-            <FormControl margin="normal" required fullWidth>
-                <InputLabel htmlFor="email">Email</InputLabel>
-                <Input 
-                    id="email"
-                    name="email"
-                    autoComplete="off"
-                    autoFocus
-                    value={email}
-                    onChange={e => setEmail(e.target.value)}
-                />
-            </FormControl>
-            <FormControl margin="normal" required fullWidth>
-                <InputLabel htmlFor="password">Password</InputLabel>
-                <Input  
-                    id="password"
-                    name="password"
-                    autoComplete="off"
-                    autoFocus
-                    value={password}
-                    onChange={e => setPassword(e.target.value)}
-                />
-            </FormControl>
-            <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                color="primary"
-                onClick={onRegister}
-            >
-                Submit
-            </Button>
-        </form>
-    )
-    async function onRegister() {
-        try {
-            await firebase.register(name, email, password)
-            props.history.replace('/dashboard')
-        } catch(error) {
-            alert(error.message)
-        }
+  // const onSubmit = e => {
+  //     e.preventDefault()
+  //     firebase
+  //         .firestore()
+  //         .collection("account")
+  //         .add({
+  //             name,
+  //             email,
+  //             password,
+  //         })
+  //         .then(() => setName(""), setEmail(""), setPassword(""))
+  // }
+  return (
+    <form onSubmit={(e) => e.preventDefault() && false}>
+      <Typography component="h1" variant="h5">
+        Register Account
+      </Typography>
+      <FormControl margin="normal" required fullWidth>
+        <InputLabel htmlFor="name">Name</InputLabel>
+        <Input
+          id="name"
+          name="name"
+          autoComplete="off"
+          autoFocus
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </FormControl>
+      <FormControl margin="normal" required fullWidth>
+        <InputLabel htmlFor="email">Email</InputLabel>
+        <Input
+          id="email"
+          name="email"
+          autoComplete="off"
+          autoFocus
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </FormControl>
+      <FormControl margin="normal" required fullWidth>
+        <InputLabel htmlFor="password">Password</InputLabel>
+        <Input
+          id="password"
+          name="password"
+          autoComplete="off"
+          autoFocus
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </FormControl>
+      <Button
+        type="submit"
+        fullWidth
+        variant="contained"
+        color="primary"
+        onClick={onRegister}
+      >
+        Submit
+      </Button>
+    </form>
+  );
+  async function onRegister() {
+    try {
+      await firebase.register(name, email, password);
+      props.history.replace("/dashboard");
+    } catch (error) {
+      alert(error.message);
     }
-}
-export default Signup;
+  }
+};
+export default withRouter(withStyles(styles)(Signup));
