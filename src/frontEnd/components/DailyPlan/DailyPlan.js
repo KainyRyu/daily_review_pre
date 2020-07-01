eb108d990156876a7ada7dd97abf875e12e6ebb6
Kainy Ryu <queueing2020@gmail.com>
2020-07-01 23:34:17 +0900

    create dailyplan component

---
+++ src/frontEnd/components/DailyPlan/DailyPlan.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./dailyPlan.css";
+
+export default function DailyPlan() {
+  return (
+      
+    <table className="dailyplan-table">
+      <tr>
+        <td>00 ~ </td>
+        <td> Sleeping </td>
+      </tr>
+    </table>
+  );
+}
--- src/frontEnd/components/Home/Home.js
+++ src/frontEnd/components/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Priority from '../Priority/Priority'
 import CheckList from '../CheckList/CheckList'
+import DailyPlan from '../DailyPlan/DailyPlan'
 
 export default function Home() {
 
     return (
         <>
             <Priority />
+            <DailyPlan />
             <CheckList />
         </>
     )
