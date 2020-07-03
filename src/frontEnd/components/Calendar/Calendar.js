ed34aeadb90cd8cc6685d8e826ded771b1cf21fd
Kainy Ryu <queueing2020@gmail.com>
2020-07-03 13:49:43 +0900

    invoke react calendar

---
+++ src/frontEnd/components/Calendar/Calendar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Rcal from 'react-calendar'
+
+export default function Calendar() {
+
+
+
+
+
+    return (
+        <div>
+            <h1>Calendar</h1>
+            <div id="calendar">
+            <Rcal />
+            </div>
+        </div>
+    )
+}
