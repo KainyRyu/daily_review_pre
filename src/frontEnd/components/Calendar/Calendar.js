9152c4e5e3e6de1c8b09f4b198d0c18db4d046e3
Kainy Ryu <kainy2109@gmail.com>
2020-07-03 15:02:07 +0900

    change the account?

--- src/frontEnd/components/Calendar/Calendar.js
+++ src/frontEnd/components/Calendar/Calendar.js
@@ -1,18 +1,13 @@
-import React from 'react'
-import Rcal from 'react-calendar'
+import React from "react";
+import Rcal from "react-calendar";
 
 export default function Calendar() {
-
-
-
-
-
-    return (
-        <div>
-            <h1>Calendar</h1>
-            <div id="calendar">
-            <Rcal />
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Calendar and some changes working?</h1>
+      <div id="calendar">
+        <Rcal />
+      </div>
+    </div>
+  );
 }
