99a6ce15968f8d674921d0cc848c1480dadcd021
Kainy Ryu <kainy2109@gmail.com>
2020-07-10 00:55:30 +0900

    creating Edit plan component

--- src/frontEnd/components/DailyPlan/DailyPlan.js
+++ src/frontEnd/components/DailyPlan/DailyPlan.js
@@ -17,60 +17,7 @@ export default function DailyPlan() {
     return `${hour} : ${minute < 10 ? `0${minute}` : minute}`;
   }
 
-  let displaytime = [
-    {
-      time: 0,
-    },{
-      time: 1
-    },{
-      time: 2,
-    },{
-      time: 3,
-    },{
-      time: 4,
-    },{
-      time: 5,
-    },{
-      time: 6,
-    },{
-      time: 7,
-    },{
-      time: 8,
-    },{
-      time: 9,
-    },{
-      time: 10,
-    },{
-      time: 11,
-    },{
-      time: 12,
-    },{
-      time: 13,
-    },{
-      time: 14,
-    },{
-      time: 15,
-    },{
-      time: 16,
-    },{
-      time: 17,
-    },{
-      time: 18,
-    },{
-      time: 19,
-    },{
-      time: 20,
-    },{
-      time: 21,
-    },{
-      time: 22,
-    },{
-      time: 23,
-    },
-  ];
-//   let event = displaytime.map((display) => ({ time: display.time }));
   function timeTable() {
-
     let displayHours = timeSlot().map((event, index) => (
       <tr>
         {index < 10 ? (
--- src/frontEnd/components/DailyPlan/EditPlan.js
+++ src/frontEnd/components/DailyPlan/EditPlan.js
@@ -1,9 +1,16 @@
 import React, { useState } from 'react'
 
 export default function EditPlan(props) {
+    const [title, setTitle] = useState('New Event');
+    const [startTime,setStartTime] = useState(0);
+    const [endTime,setEndTime] = useState(0);
+    const [due,setDue] = useState(0);
+    const [priority, setPriority] = useState('');
+    const [memo, setMemo] = useState('')
+
     return (
         <form>
-            
+
         </form>
     )
 }
