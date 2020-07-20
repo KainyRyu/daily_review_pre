927489d45cf2cd7d6c655d62a33ada1174a99942
Kainy Ryu <kainy2109@gmail.com>
2020-07-20 16:51:45 +0900

    add inputs' function

--- src/frontEnd/components/DailyPlan/EditPlan.js
+++ src/frontEnd/components/DailyPlan/EditPlan.js
@@ -2,23 +2,24 @@ import React, { useState } from 'react';
 import './editplan.css';
 
 export default function EditPlan(props) {
-    const [title, setTitle] = useState('New Event');
+    const [title, setTitle] = useState('');
     const [startTime,setStartTime] = useState(0);
     const [endTime,setEndTime] = useState(0);
     const [due,setDue] = useState(0);
     const [priority, setPriority] = useState('');
     const [memo, setMemo] = useState('')
 
+
     return (
         <form id="edit-form">
             <div>
-                <input type="text" placeholder="Title" value={title}/>
+                <input type="text" placeholder="New Event" onChange={(e) => setTitle(e.target.value)} value={title}/>
             </div>
             <div>
-                <label>Start time </label><input type="time" value={startTime}/>
+                <label>Start time </label><input type="time" onChange={(e) => setStartTime(e.target.value)} value={startTime}/>
             </div>
             <div>
-                <label>End time </label><input type="time" value={endTime}/>
+                <label>End time </label><input type="time" onChange={(e) => setEndTime(e.target.value)} value={endTime}/>
             </div>
             <div>
                 Repeat
