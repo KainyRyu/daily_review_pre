a63b38e381dd329382a6900e6481098769bf8941
Kainy Ryu <kainy2109@gmail.com>
2020-07-24 11:38:42 +0900

    Done with frontend for now?

--- src/frontEnd/components/Home/Home.js
+++ src/frontEnd/components/Home/Home.js
@@ -1,16 +1,12 @@
 import React from 'react'
-import DailyPlan from '../DailyPlan/DailyPlan'
 import Priority from '../Priority/Priority'
-import Review from '../DailyPlan/Review'
 import './home.css'
 
 export default function Home() {
 
     return (
         <div id="home">
-            <Review />
             <Priority />
-            <DailyPlan />
         </div>
     )
 }
