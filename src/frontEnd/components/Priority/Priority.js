4cc93968c0b63eea3d48b00846610d9268b013e8
Kainy Ryu <kainy2109@gmail.com>
2020-07-28 01:12:35 +0900

    adding todo button is not working

--- src/frontEnd/components/Priority/Priority.js
+++ src/frontEnd/components/Priority/Priority.js
@@ -2,37 +2,34 @@ import React, { useState } from "react";
 import "./priority.css";
 
 export default function Priority() {
-  const [red, setRed] = useState([]);
-  const [orange, setOrange] = useState([]);
-  const [yellow, setYellow] = useState([]);
-  const [gray, setGray] = useState([]);
-  const [item, setItem] = useState("");
+  const [title, setTitle] = useState("");
   const [isUrgent, setIsUrgent] = useState(true);
   const [isSignificant, setIsSignificant] = useState(true);
-  const [newTodoItem, setNewtodoItem] = useState([]);
+  const [todoList, setTodoList] = useState([]);
 
-
-    function addTodo() {
-        setNewtodoItem(newTodoItem.concat({title: item, isUrgent: true, imImportant: true}))
-        console.log(newTodoItem)
-    }
-
-    function deleteTodo(e) {
-        setGray(gray.filter((todo) => todo.e !== e));
-        console.log(gray);
-    }
-
-
-    const newTodo = e => setItem(e.target.value);
+    
+    const getTitle = e => setTitle(e.target.value);
     const urgentSelection = e => setIsUrgent((e.target.value) !== "true" ? false : true);
     const significatSelection = e => setIsSignificant((e.target.value) === "true" ? true : false);
+    
+    function addTodo() {
+        setTodoList(todoList.concat({title: title, urgency: isUrgent, importance: isSignificant}))
+    }
 
+    function getRed() {
+        todoList
+            .filter(todo => todo.urgency)
+            .filter(todo => todo.importance)
+            .map((todo,index) => {
+                return <li key={index}>{todo.title}</li>
+            })
+    }
 
   return (
     <div className="main">
-        <form id="add_input">
+        <div id="add_input">
             <div className="input_wrapper">
-                <input className="text_input" type="text" value={item} onChange={newTodo} />
+                <input className="text_input" type="text" value={title} onChange={getTitle} />
                 <select
                     id="priority_selection"
                     value={isUrgent}
@@ -51,52 +48,37 @@ export default function Priority() {
                 </select>
             </div>
             <button onClick={addTodo}>
-            +
+            Click
             </button>
-        </form>
+        </div>
 
       <div className="priority-box red">
         <h3>Urgent & Significant</h3>
         <ul>
-          {red.map((todo, index) => (
-            <li key={index}>
-              <span>{todo}</span>
-              <button onClick={deleteTodo(todo)}>X</button>
-            </li>
-          ))}
+          {getRed}
         </ul>
       </div>
       <div className="priority-box orange">
         <h3>Urgent & Insignificant</h3>
         <ul>
-          {orange.map((todo, index) => (
+          {/* {orange.map((todo, index) => (
             <li key={index}>
               <span>{todo}</span>
               <button>X</button>
             </li>
-          ))}
+          ))} */}
         </ul>
       </div>
       <div className="priority-box yellow">
         <h3>Not Urgent & Significant</h3>
         <ul>
-          {yellow.map((todo, index) => (
-            <li key={index}>
-              <span>{todo}</span>
-              <button>X</button>
-            </li>
-          ))}
+
         </ul>
       </div>
       <div className="priority-box gray">
         <h3>Not Urgent & Insignificant</h3>
         <ul>
-          {gray.map((todo, index) => (
-            <li key={index}>
-              <span>{todo}</span>
-              <button>X</button>
-            </li>
-          ))}
+
         </ul>
       </div>
     </div>
