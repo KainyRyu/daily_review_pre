bed6fb7caf1531530e0d4208b3b1ffe005c33ddd
Kainy Ryu <kainy2109@gmail.com>
2020-07-30 16:55:10 +0900

    some changes on review component

--- src/frontEnd/components/DailyPlan/Review/Review.js
+++ src/frontEnd/components/DailyPlan/Review/Review.js
@@ -3,20 +3,34 @@ import '../dailyPlan.css'
 import './review.css'
 
 //relate with <Priority todo={todo}/> - Else
-//send percentage, elseTitle, PorN
+//send percentage, elseTitle, positive%, negative%
+//if productivity !== 100% ? Else default 100 - productivity%, Negative, placeholder="Something Else"
 
+function AddElses() {
+    return (
+        <div className="add-else-warpper">
+            <div className="review_input_wrapper" style={{ display: "flex"}}>
+                <input className="review_input" type="text"/>
+                <input className="percentages" type="number"/>%
+            </div>
+            <div className="review_input_wrapper" style={{ display: "flex"}}>
+                <span style={{flex: 1}}>related to</span>
+                <select id="review_related_select">
+                    <option>
+                    </option>
+                </select>
+            </div>
+            <div className="checkboxes">
+                <input type="checkbox" className="checkbox"/>
+                <input type="checkbox" className="checkbox"/>
+            </div>
+        </div>
+    )
+}
 export default function Review() {
-    const [productivity, setProductivity] = useState()
+    const [productivity, setProductivity] = useState(0)
     const [somethingElse, setSomethingElse] = useState([])
 
-    function addElses() {
-        return (
-            <form className="input_wrapper">
-                <input className="text_input" type="text"/>
-                <input className="percentages" type="number" value={percent} onChange={} />
-            </form>
-        )
-    }
     
 
     return (
@@ -25,35 +39,31 @@ export default function Review() {
                 <h3>time - time</h3>
                 <div className="review_input_wrapper">
                     <input className="review_input" type="text" placeholder="" />
-                    <input className="percentages" type="number" placeholder={100}/>%
+                    <option>
+                        <select>100</select>
+                        <select>90</select>
+                        <select>80</select>
+                        <select>70</select>
+                        <select>60</select>
+                        <select>50</select>
+                        <select>40</select>
+                        <select>30</select>
+                        <select>20</select>
+                        <select>10</select>
+                        <select value="direct"></select>
+                    </option>
+                    <input type="text" id="selboxDirect" name="selboxDirect"/>
+                    <input className="percentages" type="number" placeholder="100"/>%
                 </div>
                 <input className="review_button" type="submit" value="Done" />
             </div>
             <div>
-                <div style={{ display: "flex"}}>
+                <div>
                     <h2>Else</h2>
-                    {
-                        //if button clicked add
-                    }
-                    <button onClick={addElses}>+</button>
+                    <AddElses />
+                    <button>+</button>
                 </div>
-                <div className="add-else-warpper">
-                    <div className="review_input_wrapper" style={{ display: "flex"}}>
-                        <input className="review_input" type="text"/>
-                        <input className="percentages" type="number"/>%
-                    </div>
-                    <div className="review_input_wrapper" style={{ display: "flex"}}>
-                        <span style={{flex: 1}}>related to</span>
-                        <select id="review_related_select">
-                            <option>
-                            </option>
-                        </select>
-                    </div>
-                    <div className="checkboxes">
-                        <input type="checkbox" className="checkbox"/>
-                        <input type="checkbox" className="checkbox"/>
-                    </div>
-                </div>
+                
             </div>
 
         </div>
