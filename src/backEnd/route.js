521c75f32f85fee1dcb0b6cfe82a043644b40237
Kainy Ryu <kainy2109@gmail.com>
2020-08-23 00:28:29 +0900

    Udemy95, Handling errors for unsupported routes

--- src/backEnd/route.js
+++ src/backEnd/route.js
@@ -1,51 +1,13 @@
 const express = require('express');
 
-const HttpError = require('../../models/http-error')
+const placesControllers = require('../../controllers/places-controller');
 
 const router = express.Router();
 
-const DUMMY_place = [
-    {
-        id: 'p1',
-        title: 'Empire State Building',
-        description: 'One of the most famous sky scrapers in the world',
-        location: {
-            lat: 40.7484474,
-            lng: -73.9871516
-        },
-        address: '20 W 34th St, New York, NY 10001',
-        creator: 'u1'
-    }
-]
-router.get('/:pid', (req, res, next) => {
-    const placeId = req.params.pid;
-    
-    const place = DUMMY_place.find(p => {
-        return p.id == placeId
-    });
+router.get('/:pid', placesControllers.getPlaceById);
 
-    if (!place){
-        throw new HttpError('Could not find a place for the provided id.', 404)
-    } 
+router.get('/user/:uid', placesControllers.getPlaceByUserId);
 
-    res.json({place});
-});
-
-router.get('/user/:uid', (req, res, next) => {
-    const userId = req.params.uid;
-    
-    const place = DUMMY_place.find(p => {
-        return p.creator === userId;
-    });
-
-    if (!place){
-        return next(
-            new Error('Could not find a place for the provided user id.'), 404
-        );
-
-    } 
-
-    res.json({ place })
-});
+router.post('/', placesControllers.createPlace);
 
 module.exports = router;
