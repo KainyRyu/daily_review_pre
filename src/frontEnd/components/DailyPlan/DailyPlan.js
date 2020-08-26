b1e2d9e84ad955cff1f806917831a08b21a537bd
clianor <ejrgus94160@gmail.com>
2020-08-26 15:58:01 +0900

    Refactor: Changed to use Context API
    Signed-off-by: clianor <ejrgus94160@gmail.com>

--- src/frontEnd/components/DailyPlan/DailyPlan.js
+++ src/frontEnd/components/DailyPlan/DailyPlan.js
@@ -1,37 +1,15 @@
-import React, { useState } from "react";
+import React, {useContext, useState} from "react";
 import EditPlan from "./EditPlan";
 import Review from "./Review/Review";
+import {MyContext} from "../../context/timeSlotsContext";
 import "./dailyPlan.css";
 
 
 export default function DailyPlan() {
   const [currentTime, setCurrentTime] = useState(0);
-  const [timeslots, setTimeslots] = useState([
-    {time: '00', title: '', review: '' },
-    {time: '01', title: '', review: '' },
-    {time: '02', title: '', review: 'Sleep' },
-    {time: '03', title: '', review: 'Sleep' },
-    {time: '04', title: '', review: 'Sleep' },
-    {time: '05', title: '', review: '' },
-    {time: '06', title: '', review: '' },
-    {time: '07', title: '', review: '' },
-    {time: '08', title: '', review: 'Yoga' },
-    {time: '09', title: '', review: '' },
-    {time: '10', title: '', review: '' },
-    {time: '11', title: '', review: '' },
-    {time: '12', title: '', review: 'Reading' },
-    {time: '13', title: '', review: '' },
-    {time: '14', title: '', review: '' },
-    {time: '15', title: '', review: '' },
-    {time: '16', title: '', review: '' },
-    {time: '17', title: '', review: '' },
-    {time: '18', title: '', review: '' },
-    {time: '19', title: '', review: '' },
-    {time: '20', title: '', review: '' },
-    {time: '21', title: '', review: '' },
-    {time: '22', title: '', review: '' },
-    {time: '23', title: '', review: '' },
-  ])
+
+  const {state, dispatch} = useContext(MyContext);
+  const {timeslots} = state;
 
   let newDate = new Date();
   let hour = newDate.getHours();
@@ -42,6 +20,7 @@ export default function DailyPlan() {
   }
 
   function totalProductivity() {
+    console.log(timeslots)
     const reviews = timeslots.map(timeslot => timeslot.review)
     const reviewSet = new Set(reviews)
     reviewSet.delete("")
@@ -51,13 +30,10 @@ console.log('total review else = ',totalProductivity().length)
 console.log(timeslots)
 
   function changeTimeSlot(newTimeSlot) {
-    const newTimeSlots = timeslots.map((timeslot) =>
-      (timeslot.time >= newTimeSlot.starts && timeslot.time <= newTimeSlot.ends) ? {
-        ...timeslot,
-        "title": newTimeSlot.title
-      } : timeslot
-    )
-    setTimeslots(newTimeSlots);
+    dispatch({
+      type: "CHANGE_TIMESLOT",
+      newTimeSlot: newTimeSlot
+    })
   }
 
   function timeTable() {
