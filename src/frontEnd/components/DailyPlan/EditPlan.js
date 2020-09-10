ec3d2e5e0794c07b44c2d6f3b5996710892ed34a
Kainy Ryu <kainy2109@gmail.com>
2020-09-11 03:27:57 +0900

    changed the buttons' style

--- src/frontEnd/components/DailyPlan/EditPlan.js
+++ src/frontEnd/components/DailyPlan/EditPlan.js
@@ -77,61 +77,63 @@ export default function EditPlan({ id }) {
     // }
 
     return (
-        <form id="edit-form" >
-            <div className="input-wrapper">
-                <input 
-                    id="new-event-input"
-                    type="text" 
-                    placeholder="New Event" 
-                    onChange={getTitle} 
-                    value={newEvent.title}
-                />
+        <form >
+            <div  id="edit-form">
+                <div className="input-wrapper">
+                    <input 
+                        id="new-event-input"
+                        type="text" 
+                        placeholder="New Event" 
+                        onChange={getTitle} 
+                        value={newEvent.title}
+                    />
+                </div>
+                <div className="input-wrapper">
+                    <label>Starts </label>
+                    <select 
+                        className="edit-plan-time"
+                        onChange={getStarts}
+                        value={newEvent.starts}
+                    >
+                        {
+                            timeslots.map((hour, index) => {
+                                return <option key={index}>{index < 10 ? `0${index}` : index}</option>
+                            })
+                        }
+                    </select>
+                </div>
+                <div className="input-wrapper">
+                    <label>Ends </label>
+                    <select 
+                        className="edit-plan-time"
+                        onChange={getEnds}
+                        value={newEvent.ends}
+                    >     
+                        {
+                            timeslots.map((hour, index) => {
+                                return <option key={index}>{index < 10 ? `0${index}` : index}</option>
+                            })
+                        }
+                    </select>
+                </div>
+                {/* <div className="input-wrapper">
+                    Repeat
+                </div> */}
+                <div className="input-wrapper">
+                    <span>Alert</span><span>></span>
+                </div>
+                {/* <div className="input-wrapper">
+                    <h3 style={{margin: 0}}>BOLD</h3>
+                    <label class="switch">
+                        <input type="checkbox" />
+                        <span class="slider round"></span>
+                    </label>
+                </div> 
+                <div>
+                    <textarea placeholder="Notes" rows="4" onChange={getMemo}>
+                    </textarea>
+                </div> */}
             </div>
-            <div className="input-wrapper">
-                <label>Starts </label>
-                <select 
-                    className="edit-plan-time"
-                    onChange={getStarts}
-                    value={newEvent.starts}
-                >
-                    {
-                        timeslots.map((hour, index) => {
-                            return <option key={index}>{index < 10 ? `0${index}` : index}</option>
-                        })
-                    }
-                </select>
-            </div>
-            <div className="input-wrapper">
-                <label>Ends </label>
-                <select 
-                    className="edit-plan-time"
-                    onChange={getEnds}
-                    value={newEvent.ends}
-                >     
-                    {
-                        timeslots.map((hour, index) => {
-                            return <option key={index}>{index < 10 ? `0${index}` : index}</option>
-                        })
-                    }
-                </select>
-            </div>
-            {/* <div className="input-wrapper">
-                Repeat
-            </div> */}
-            <div className="input-wrapper">
-                <span>Alert</span><span>></span>
-            </div>
-            {/* <div className="input-wrapper">
-                <h3 style={{margin: 0}}>BOLD</h3>
-                <label class="switch">
-                    <input type="checkbox" />
-                    <span class="slider round"></span>
-                </label>
-            </div> 
-            <div>
-                <textarea placeholder="Notes" rows="4" onChange={getMemo}>
-                </textarea>
-            </div> */}
             <button className="submit-btn" onClick={submitHandler}>Submit</button>
         </form>
     )
