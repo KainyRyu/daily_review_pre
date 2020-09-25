921683ef4510cd55fd636491a26d3122f08602e7
Kainy Ryu <kainy2109@gmail.com>
2020-09-25 21:48:41 +0900

    (backend) import mongodb

--- controllers/mongo.js
+++ controllers/mongo.js
@@ -1,3 +1,7 @@
+const MongoClient = require('mongodb').MongoClient;
+
+const url = 'mongodb+srv://kainy_admin:6PJt5uISlwnILn08@cluster0.osruv.mongodb.net/Daily_Review?retryWrites=true&w=majority';
+
 const createProduct = async (req, res, next) => {
     
 };
