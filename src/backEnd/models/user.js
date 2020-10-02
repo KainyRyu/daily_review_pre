f378914d48ab926db22aac635de80b256727ff18
Kainy Ryu <kainy2109@gmail.com>
2020-10-02 22:42:20 +0900

    (db)Connect frined-user data

--- src/backEnd/models/user.js
+++ src/backEnd/models/user.js
@@ -7,7 +7,9 @@ const userSchema = new Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true, minlength: 6 },
-    location: { type: String, required: true }
+    // location: { type: String, required: true },
+    friends: [{ type: mongoose.Types.ObjectId, required: true, ref: 'Friend' }]
 });
 
 module.exports = mongoose.model('User', userSchema);
+
